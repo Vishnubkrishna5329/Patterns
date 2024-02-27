f8ce8ca8ec525e906dd65242c18de1f83c0ebbbe
Vishnu B <vishnubkrishna471@gmail.com>
2024-02-27 21:46:12 +0530

    bug resolved

--- Pattern_13_Mirror_Pattern.java
+++ Pattern_13_Mirror_Pattern.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Pattern_13_Mirror_Pattern {
     
-    public void main(String[] args){
+    public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
